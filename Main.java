f86d65749bb69d86ae7cfcf81884f1fcb1b85741
Diana Islava <dianaislava@gmail.com>
2026-09-12 14:24:36 -0600

    Added authorship, desc, changelog and started the main class for lab2

---
+++ Main.java
@@ -0,0 +1,12 @@
+/*
+Main Author: Diana S Islava
+Primary controller for chess movement verifier
+Changelog
+12/09 - Created main class
+ */
+import java.util.Scanner;
+public class Main{
+    public static void main(String[] args) {
+        
+    }
+}
