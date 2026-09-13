bdf661e685d5e7333720290f9b574d0973d18dc4
Diana Islava <dianaislava@gmail.com>
2026-09-12 21:06:14 -0600

    removed enum for xcoords, added switch to create pieces using enum vals as cases

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@
 Main Author: Diana S Islava
 Primary controller for chess movement verifier
 Changelog
-12/09 - Created main class
+12/09 - Created main class, temp enum for pieces (testing), switch to get specified piece
  */
 import java.util.Scanner;
 public class Main{
@@ -15,33 +15,48 @@ public class Main{
         PAWN
     }
 
-    enum X_Coord{
-        a,b,c,d,e,f,g,h
-    }
-
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.print("This programs seeks to help a user check if a given chess move is valid.");
         System.out.println("To utilize this program, typed the specified information of the piece");
         System.out.println("Then, push enter to move to the next requirement");
+
         boolean start=true;
+        PieceType piece_name;
+
         while (start){
             System.out.println("Please type the chesspiece ");
-                String piece_name=input.nextLine();
-                piece_name.toUpperCase();
-                switch (piece_name) {
-                    case "PAWN":
-                        //PieceType new_piece=PieceType.PAWN;
-                    break;
-                    default:
-                    break;
-                }
+                String name=input.nextLine();
+                piece_name = PieceType.valueOf(name);
             System.out.println("Please type the color of the piece ");
                 String color=input.nextLine();
             System.out.println("Please enter the row at which the piece is at");
                 int pos_y=input.nextInt();
             System.out.println("Please enter the column at which the piece is at");
                 char pos_x=input.nextLine().charAt(0);
+            
+            switch (piece_name) {
+                    case PieceType.PAWN:
+                        //Pawn pawn=new Pawn(attributes)
+                    break;
+                    case PieceType.ROOK:
+                        //Rook rook=new Rook(attributes)
+                    break;
+                    case PieceType.BISHOP:
+                        //Bishop bishop=new Bishop(attributes)
+                    break;
+                    case PieceType.KNIGHT:
+                        //Knight knight=new Knight(attributes)
+                    break;
+                    case PieceType.QUEEN:
+                        //Queen queen=new Queen(attributes)
+                    break;
+                    case PieceType.KING:
+                        //King king=new King(attributes)
+                    break;
+                    default:
+                    break;
+                }
             
         }
         input.close();
