080fda0ce8176b4d77f8adf31f91fd10fda0fdcb
Andresiglesi3 <amiglesias@miners.utep.edu>
2026-09-14 18:28:52 -0600

    Pieces classes
    Finished the classes, for the chess pieces Bishop, King, Knight, and Pawn.

---
+++ Knight.java
@@ -0,0 +1,67 @@
+public class Knight{
+    // all of the attributes of the Knight piece
+    private String piece_name;
+    private String color;
+    private char column;
+    private int row;
+
+    // Empty constructor
+    public Knight(){
+        this.piece_name = PieceType.KNIGHT.name();
+        this.color = "White";
+        this.column = 'A';
+        this.row = 1;
+    }
+
+    // Constructor with parameters
+    public Knight(String color, char column, int row){
+        this.piece_name = PieceType.KNIGHT.name();
+        this.color = color;
+        this.column = column;
+        this.row = row;
+    }
+    // Getters 
+
+    public String getColor() {
+        return color;
+    }
+
+    public char getColumn() {
+        return column;
+    }
+
+    public int getRow() {
+        return row;
+    }
+    // Setters
+
+    public void setColumn(char column) {
+        this.column = column;
+    }
+
+    public void setRow(int row){
+        this.row = row;
+
+    }
+    
+
+    //Methods
+
+    public boolean verifyTarget(char targetColumn, int targetRow) {
+        int columnDifference = Math.abs(targetColumn - this.column);
+        int rowDifference = Math.abs(targetRow - this.row);
+        
+        // Check if it moves at all
+        if (columnDifference == 0 && rowDifference == 0) {
+            return false;
+        }
+        // Checks if it moves in an L shape
+        if ((columnDifference == 2 && rowDifference ==1) || (columnDifference == 1 && rowDifference == 2)) {
+            return true;
+        }
+        return false;
+    }
+
+
+
+}
